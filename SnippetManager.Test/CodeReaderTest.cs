4e55c237e44defae2afc24d494e0d313ffb96170
Tinpuny <tinpuny44@gmail.com>
2018-02-20 23:06:44 +0900

    feat: コードからスニペット部分を取り出すクラスの作成

---
+++ SnippetManager.Test/CodeReaderTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace SnippetManager.Test
+{
+    public class CodeReaderTest
+    {
+        [Fact]
+        public void GetSimpleSnippetInfo()
+        {
+            var codeLines = new[]
+            {
+                "XXX",
+                "//$Hoge",
+                "YYY",
+                "//$Hoge",
+                "ZZZ",
+            };
+
+            var infos = new CodeReader(codeLines).GetSnippetInfos().Single();
+            Assert.Equal("Hoge", infos.Title);
+            Assert.Equal(new[] {"YYY"}, infos.CodeLines);
+        }
+
+        [Fact]
+        public void GetMultipleSnippetInfos()
+        {
+            var codeLines = new[]
+            {
+                "XXX",
+                "//$Hoge",
+                "YYY",
+                "//$Hoge",
+                "ZZZ",
+                "//$Fuga",
+                "AAA",
+                "BBB",
+                "CCC",
+                "//$Fuga",
+                "//$Piyo",
+                "//$Piyo",
+            };
+
+            var infos = new CodeReader(codeLines).GetSnippetInfos().ToArray();
+            Assert.Equal(new[]{"Hoge", "Fuga", "Piyo"}, infos.Select(sn => sn.Title));
+
+            var exp = new[]
+            {
+                new[] {"YYY"},
+                new[] {"AAA", "BBB", "CCC"},
+                new string[0],
+            };
+            Assert.Equal(exp, infos.Select(sn => sn.CodeLines));
+        }
+
+        [Fact]
+        public void GetNonEndSnippetInfo()
+        {
+            var codeLines = new[]
+            {
+                "XXX",
+                "//$Hoge",
+                "YYY",
+                "ZZZ",
+            };
+
+            var infos = new CodeReader(codeLines).GetSnippetInfos().Single();
+            Assert.Equal(new [] {"YYY", "ZZZ"}, infos.CodeLines);
+        }
+    }
+}
---
+++ SnippetManager/CodeReader.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SnippetManager
+{
+    public class Snippet
+    {
+        public string Title { get; }
+        public IReadOnlyCollection<string> CodeLines { get; }
+
+        public Snippet(string title, IReadOnlyCollection<string> codeLines)
+        {
+            Title = title;
+            CodeLines = codeLines;
+        }
+    }
+
+    public class CodeReader
+    {
+        const string TagPoint = "//$";
+
+        readonly IReadOnlyCollection<string> codeLines;
+
+        public CodeReader(string[] codeLines)
+        {
+            this.codeLines = codeLines;
+        }
+
+        public IEnumerable<Snippet> GetSnippetInfos()
+        {
+            var usedRows = new HashSet<int>();
+
+            foreach (var (line, i) in codeLines.Select((line, i) => (line, i)))
+            {
+                if (IsSnippetTag(line, out var title) && !usedRows.Contains(i))
+                {
+                    yield return GetSnippetInfo(title, i + 1, out var end);
+                    usedRows.Add(end);
+                }
+            }
+        }
+
+        Snippet GetSnippetInfo(string title, int start, out int end)
+        {
+            var snippetLines = new List<string>();
+
+            foreach (var (line, i) in codeLines.Skip(start).Select((line, i) => (line, i)))
+            {
+                if (IsSnippetTag(line, out var _title) && _title == title)
+                {
+                    end = i + start;
+                    return new Snippet(title, snippetLines);
+                }
+
+                snippetLines.Add(line);
+            }
+
+            end = codeLines.Count;
+            return new Snippet(title, snippetLines);
+        }
+
+        bool IsSnippetTag(string line, out string title)
+        {
+            var trim = line.Trim(' ', '\t');
+
+            if (trim.StartsWith(TagPoint))
+            {
+                title = trim.Remove(0, TagPoint.Length).Trim(' ');
+                return true;
+            }
+
+            title = null;
+            return false;
+        }
+    }
+}
+
